a677937cf215063862399537fa899394afc0e748
colonnam <colonna_m15@yahoo.com>
2021-11-19 09:14:58 +0100

    1.0 standard ok

---
+++ standard/src/test/java/com/meteo/standard/StandardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meteo.standard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
