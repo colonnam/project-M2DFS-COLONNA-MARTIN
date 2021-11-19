b0c643adebe5074b35e91f99f40d22abc48d90af
colonnam <colonna_m15@yahoo.com>
2021-11-19 23:38:59 +0100

    7 monitoring eureka

--- standard/src/main/java/com/meteo/standard/StandardApplication.java
+++ standard/src/main/java/com/meteo/standard/StandardApplication.java
@@ -4,12 +4,13 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableHystrixDashboard
-
+@EnableEurekaClient
 public class StandardApplication {
 	@HystrixCommand(fallbackMethod ="myFallbackMethod")
 	public static void main(String[] args) {
--- standard/src/main/java/com/meteo/standard/util/SpringEurekaClientMeteoServiceInfoContributor.java
+++ standard/src/main/java/com/meteo/standard/util/SpringEurekaClientMeteoServiceInfoContributor.java
@@ -15,7 +15,7 @@ public class SpringEurekaClientMeteoServiceInfoContributor implements InfoContri
     @Override
     public void contribute(Info.Builder builder) {
         builder.withDetail("details",
-                Collections.singletonMap("description", "This is the School service, which is discovery server aware, and this service will Call Student Microservice, fro student details, which is again dicovery server aware!!! "));
+                Collections.singletonMap("description", "This is the meteo service, which is discovery server aware, and this service will Call Student Microservice, fro student details, which is again dicovery server aware!!! "));
     }
 
 }
